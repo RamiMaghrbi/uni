fe62e053be7c3fe2ab22fcb882d38d43fb56e6a2
FaisalAlomari <s444005427@st.uqu.edu.sa>
2024-04-08 18:29:27 +0300

    fuck you

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,32 +1,39 @@
-import side from "../Assets/aiu-1.jpg";
-const colors = {
-  primary: "#060606",
-  background: "#E0E0E0",
-  dusabled: "#D9D9D9",
-};
-const login = () => {
+import React, { useState } from 'react';
+
+
+function LoginForm() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberPassword, setRememberPassword] = useState(false);
+
+  const handleLogin = () => {
+    // Handle login
+  };
+
   return (
-    // <div className="w-full min-h-screen flex items-start">
-    //   <div className=" relative w-1/3 h-full flex flex-col ">
-    //     <div className="absolute top-[25%] left-[10%] flex flex-col ">
-    //       <h1 className="text-2xl text-[#E0E0E0] font-extrabold my-4 ">
-    //         welcome to aiu
-    //       </h1>
-    //     </div>
-    //     <img src={side} className="w-full  h-full  object-cover " />
-    //   </div>
-    // </div>
-    <div
-      style={{
-        backgroundImage: { side },
-        width: "300px",
-        height: "250px",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        borderRadius: "12px",
-      }}
-    ></div>
+    <div className="container">
+      <div className="left-side">
+        {/* You can add more content here */}
+      </div>
+      <div className="right-side">
+        <form onSubmit={handleLogin}>
+          <label>
+            ID:
+            <input type="text" value={id} onChange={e => setId(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          </label>
+          <label>
+            Remember password:
+            <input type="checkbox" checked={rememberPassword} onChange={e => setRememberPassword(e.target.checked)} />
+          </label>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
   );
-};
-export default login;
+}
+
+export default LoginForm;
